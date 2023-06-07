cee3911bcffd2c0a8b3916cd2f74d03961f6d297
JoaquinTaborda <130505200+Avannz@users.noreply.github.com>
2023-06-07 10:27:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "pila.h"
 
 typedef struct
@@ -9,6 +10,8 @@ typedef struct
     int edad;
     int anio;
 } stAlumno;
+
+const int dim = 30;
 
 void cargarAlFinalArchivo();
 void mostrarArchivo();
@@ -23,7 +26,11 @@ void rangoEdad(stAlumno persona, int min, int max);
 void mayorEdad(stAlumno persona);
 void encontrarMayor (stAlumno persona);
 int anioEspecifica(stAlumno persona, int anio);
-void pasarArregloAlumnos(stAlumno alumnos[],int dim , stAlumno persona);
+int pasarArregloAlumnos(stAlumno alumnos[],int dim, stAlumno persona);
+void mostrarArreglo(stAlumno arreglo[], int validos);
+void catorce (stAlumno persona, int numero);
+void modificarAlumno(int numRegistro);
+stAlumno alterarAlumno(stAlumno persona);
 
 int main()
 {
@@ -38,75 +45,89 @@ int main()
     int max;
     int anio;
     int dim = 30;
+    int validos;
+    int numero;
 
 
     printf("*** TRABAJO PRACTICO ARCHIVOS ***");
 
 
-    /*printf("\n\n *PUNTO 1: \n");
-    cargarAlFinalArchivo();
+    /* printf("\n\n *PUNTO 1: \n");
+     cargarAlFinalArchivo();
 
-    printf("\n\n");
+     printf("\n\n");
 
-    printf(" *PUNTO 2: \n");
-    mostrarArchivo();
+     printf(" *PUNTO 2: \n");
+     mostrarArchivo();
 
-    printf("\n\n");
+     printf("\n\n");
 
-    printf(" *PUNTO 3: \n");
-    cantAlumnos = cantidadDeAlumnos();
-    printf("\n- La cantidad de alumnos registrados es de: %d ", cantAlumnos);
+     printf(" *PUNTO 3: \n");
+     cantAlumnos = cantidadDeAlumnos();
+     printf("\n- La cantidad de alumnos registrados es de: %d ", cantAlumnos);
 
-    printf("\n\n");
+     printf("\n\n");
 
-    printf("\n *PUNTO 4: \n");
-    cargarAlumnos5();
+     printf("\n *PUNTO 4: \n");
+     cargarAlumnos5();
 
-    printf("\n\n");*/
+     printf("\n\n");
 
-    printf(" *PUNTO 5: \n");
-    mostrarArchivoMod();
+     printf(" *PUNTO 5: \n");
+     mostrarArchivoMod();
 
-    printf("\n\n");
+     printf("\n\n");
 
-    /*printf(" *PUNTO 6: \n");
-    cargarUnaVez();
+     printf(" *PUNTO 6: \n");
+     cargarUnaVez();
 
-    printf("\n\n");
+     printf("\n\n");
 
-    printf(" *PUNTO 7: \n");
-    cargarPilaMayores(&pila1, persona);
-    mostrar(&pila1);
+     printf(" *PUNTO 7: \n");
+     cargarPilaMayores(&pila1, persona);
+     mostrar(&pila1);
 
-    printf("\n\n");
+     printf("\n\n");
 
-    printf(" *PUNTO 8: \n");
-    cantMayores = edadEspecifica(persona);
-    printf("La cantidad de alumnos mayores a la edad ingresada es de: %d", cantMayores);
+     printf(" *PUNTO 8: \n");
+     cantMayores = edadEspecifica(persona);
+     printf("La cantidad de alumnos mayores a la edad ingresada es de: %d", cantMayores);
 
-    printf("\n\n");
+     printf("\n\n");
 
-    printf(" *PUNTO 9: \n");
-    mostrarRangoEdad(persona);
+     printf(" *PUNTO 9: \n");
+     mostrarRangoEdad(persona);
 
-    printf("\n\n");
+     printf("\n\n");
 
-     printf(" *PUNTO 10: \n");
-     mayorEdad(persona);*/
+      printf(" *PUNTO 10: \n");
+      mayorEdad(persona);
 
-    printf(" *PUNTO 11: \n");
+     printf(" *PUNTO 11: \n");
 
-    printf("Ingresa el año de la cursada: ");
-    fflush(stdin);
-    scanf("%d", &anio);
+     printf("Ingresa el año de la cursada: ");
+     fflush(stdin);
+     scanf("%d", &anio);
 
-    anioCant = anioEspecifica(persona, anio);
-    printf("La cantidad de alumnos en ese año es: %d", anioCant);
+     anioCant = anioEspecifica(persona, anio);
+     printf("La cantidad de alumnos en ese año es: %d", anioCant);
 
     printf(" *PUNTO 12: \n");
-    pasarArregloAlumnos(alumnos, dim, persona);
+    validos = pasarArregloAlumnos(alumnos, dim, persona);
+    mostrarArreglo(alumnos, validos);
 
+    printf("\nPUNTO 14: \n");
 
+    printf("Ingresa un numero entre el 0 y 9: ");
+    fflush(stdin);
+    scanf("%i", &numero);
+
+    catorce(persona, numero);*/
+
+    printf("PUNTO 15: \n");
+    
+    modificarAlumno(2);
+    mostrarArchivo();
 
 
 }
@@ -467,7 +488,7 @@ int anioEspecifica(stAlumno persona, int anio)
     return cant;
 }
 
-void pasarArregloAlumnos(stAlumno alumnos[],int dim , stAlumno persona)
+int pasarArregloAlumnos(stAlumno alumnos[],int dim, stAlumno persona)
 {
 
     FILE *archivo;
@@ -480,12 +501,180 @@ void pasarArregloAlumnos(stAlumno alumnos[],int dim , stAlumno persona)
 
         while(!feof(archivo))
         {
+            fread(&persona, sizeof(stAlumno),1, archivo);
 
-            alumnos[i] = fread(&persona, sizeof(stAlumno),1, archivo);
+            alumnos[i] = persona;
 
             i++;
         }
         fclose(archivo);
     }
+    return i;
+}
+
+void mostrarArreglo(stAlumno arreglo[], int validos)
+{
+
+    int i = 0;
+
+    for(i = 0; i < validos; i++)
+    {
+
+        printf("\nLegajo: %i\n", arreglo[i].legajo);
+        printf("Nombre: %s\n", arreglo[i].nombreYapellido);
+        printf("Edad: %i\n", arreglo[i].edad);
+        printf("Anio: %i\n", arreglo[i].anio);
+        printf("======================");
+
+    }
+}
+
+void catorce (stAlumno persona, int numero)
+{
+
+    FILE *archivo;
+    archivo = fopen("miArchivo.bin", "rb");
+
+    if(archivo != NULL)
+    {
+
+        int cant = cantidadDeAlumnos();
+        printf("%d", cant);
+
+        if(cant >= 10)
+        {
+            while(!feof(archivo))
+            {
+                fread(&persona, sizeof(stAlumno), 1, archivo);
+                if(!feof(archivo))
+                {
+                    if(numero == persona.legajo)
+                    {
+
+                        printf("===================");
+                        printf("\nLegajo: %d", persona.legajo);
+                        printf("\nNombre: %s", persona.nombreYapellido);
+                        printf("\nEdad: %d", persona.edad);
+                        printf("\nAnio: %d\n", persona.anio);
+                        printf("===================");
+
+                    }
+
+
+                }
+
+            }
+        }
+        else
+        {
+
+            printf("No se llego a la cantidad minima de alumnos :c");
+
+        }
+
+        fclose(archivo);
+    }
+}
+
+void modificarAlumno(int numRegistro){
+
+    FILE *archivo;
+    archivo = fopen("miArchivo.bin", "r+b");
+    stAlumno persona;
+
+    if(archivo != NULL)
+    {
+        fseek(archivo,sizeof(stAlumno) * (numRegistro-1),SEEK_SET);
+        fread(&persona,sizeof(stAlumno),1,archivo);
+        printf("persona1 antes===================");
+                        printf("\nLegajo: %d", persona.legajo);
+                        printf("\nNombre: %s", persona.nombreYapellido);
+                        printf("\nEdad: %d", persona.edad);
+                        printf("\nAnio: %d\n", persona.anio);
+                        printf("===================");
+        persona = alterarAlumno(persona);
+        printf("persona1===================");
+                        printf("\nLegajo: %d", persona.legajo);
+                        printf("\nNombre: %s", persona.nombreYapellido);
+                        printf("\nEdad: %d", persona.edad);
+                        printf("\nAnio: %d\n", persona.anio);
+                        printf("===================");
+        fseek(archivo,sizeof(stAlumno) * -1, SEEK_CUR);
+        fwrite(&persona,sizeof(stAlumno),1,archivo);
+
+        fclose(archivo);
+    }
+}
+
+stAlumno alterarAlumno(stAlumno persona){
+
+    int opc;
+    stAlumno cambiar;
+
+    printf("1) Cambiar legajo\n 2)Cambiar Nombre\n 3)Cambiar edad\n 4)Cambiar año\n 5) CAMBIAR TODO\n\n Insertar: ");
+    fflush(stdin);
+    scanf("%i", &opc);
+
+    switch(opc)
+    {
+
+    case 1:
+
+        printf("Ingresa el nuevo legajo: ");
+        fflush(stdin);
+        scanf("%i", &persona.legajo);
+
+        break;
+
+
+    case 2:
+
+        printf("Ingresa el nuevo nombre: ");
+        fflush(stdin);
+        gets(persona.nombreYapellido);
+
+        break;
+
+
+    case 3:
+
+        printf("Ingresa la nueva edad: ");
+        fflush(stdin);
+        gets(persona.edad);
+
+        break;
+
+
+    case 4:
+
+        printf("Ingresa el nuevo año: ");
+        fflush(stdin);
+        scanf("%i", &persona.anio);
+
+        break;
+
+
+    case 5:
+
+        printf("Ingresa el nuevo legajo: ");
+        fflush(stdin);
+        scanf("%i", &persona.legajo);
+
+        printf("Ingresa el nuevo nombre: ");
+        fflush(stdin);
+        gets(&persona.nombreYapellido);
+
+        printf("Ingresa la nueva edad: ");
+        fflush(stdin);
+        gets(&persona.edad);
+
+        printf("Ingresa el nuevo año: ");
+        fflush(stdin);
+        gets(&persona.anio);
+
+        break;
+
+    }
+    return persona;
 }
 
